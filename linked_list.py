feb27fbcd6d87899ee139475858bd62992959c18
Jordan Brengman <jordanbrengman@gmail.com>
2014-02-19 14:14:10 -0800

    Clean up

--- linked_list.py
+++ linked_list.py
@@ -5,99 +5,75 @@ class LinkedList:
 
 	def insert(self, val):
 		'''Insert val at head of the linked list'''
-		node = Linked_node(val)
-		node.next = self.head
-		self.head = node
+		self.head, self.head.next = Linked_node(val), self.head
 
 	def pop(self):
 		'''Removes and returns the first node of the linked list.'''
 		if (self.head):
-			temp = self.head
+			node = self.head
 			self.head = self.head.next
-			return temp
+			return node
 		raise ValueError('List is empty.')
 
 	def size(self):
 		'''Returns the number of items in the linked list.'''
 		n = 0
-		if self.head:
-			node = self.head
-			n = 1
-			while (node.next):
-				node = node.next
-				n += 1
+		node = self.head
+		while (node):
+			node = node.next
+			n += 1
 		return n
 
 	def search(self, val):
 		'''Returns the node containing val.'''
-		if self.head:
-			node = self.head
+		node = self.head
+		while (node):
 			if (node.value == val):
 				return node
-			while (node.next):
-				node = node.next
-				if (node.value == val):
-					return node
+			node = node.next
 		raise ValueError('Item not found')
 
 	def remove(self, rem_node):
 		'''Removes the node rem_node if rem_node is a node in the linked list.'''
-		if self.head:
-			node = self.head
-			if (node == rem_node): 
-			# If first node of list is the node to be removed
-				self.head = self.head.next
+		node = self.head
+		if (node == rem_node):
+			self.head = node.next
+			return
+		while (node):
+			if (node.next == rem_node):
+				node.next = node.next.next
 				return
-			while (node.next):
-				if (node.next == rem_node):
-					if (node.next.next):
-					# If the node being removed is not the last item in the list 
-						node.next = node.next.next
-					else: node.next = None 
-					return
-				node = node.next
+			node = node.next
 		raise ValueError('Item not found')
 
 	def remove_by_value(self, val):
 		'''Removes the first node in the linked list whose value is val.'''
-		if self.head:
-			node = self.head
-			if (node.value == val):
-			# If first node of list is the node to be removed
-				self.head = self.head.next
+		node = self.head
+		if (node.value == val):
+			self.head = node.next
+			return
+		while (node.next):
+			if (node.next.value == val):
+				node.next = node.next.next
 				return
-			while (node.next):
-				if (node.next.value == val):
-					if (node.next.next):
-					# If the node being removed is not the last item in the list 
-						node.next = node.next.next
-					else: node.next = None 
-					return
-				node = node.next
+			node = node.next
 		raise ValueError('Item not found')
 
 	def contains(self, val):
-		if (self.head):
-			node = self.head
+		node = self.head
+		while (node):
 			if (node.value == val):
 				return True
-			while (node.next):
-				node = node.next
-				if (node.value == val):
-					return True
+			node = node.next
 		return False
 
 	def __str__(self):
 		tup = ()
-		if self.head:
-			node = self.head
-			tup = (node.value,)
-			while (node.next):
-				node = node.next
-				tup += (node.value,)
+		node = self.head
+		while (node):
+			tup += (node.value,)
+			node = node.next
 		return str(tup)
-
-
 
 
 class Linked_node:
